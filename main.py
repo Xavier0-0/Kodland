ee6a29db0d03d955e276ea23625de42bf8cb88fe
Xavier0-0 <xidziak@gmail.com>
2024-08-08 16:43:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,70 @@
-None
-baza_dannych = {
-  'LOL' : 'odpowiedź na coś zabawnego',
-  'CRINGE' : 'coś dziwnego lub wstydliwego',
-  'ROFL' : 'odpowiedź na żart',
-  'SHEESH' : 'lekka dezaprobata',
-  'CREEPY' : 'straszny, złowieszczy',
-  'AGGRO' : 'stać się agresywnym/zły'
-}
-print('Wstkie Slowa', baza_dannych)
+import discord, random, bcrypt
+from discord.ext import commands
 
-slowo = input('Napiz slowo ktore zmeiniemy')
+intents = discord.Intents.default()
 
-if slowo in baza_dannych:
-  new_value = input('Podaje nowe slowo:')
-  baza_dannych[slowo] = new_value
-  print('ASKtualizowa baza dannych: ', baza_dannych)
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Bot Gotowy!')
+    print(f'zalagowano jako {bot.user.name}')
+
+@bot.command()
+async def dodaj(ctx, a:int, b: int):
+    await ctx.send(f'{a} + {b} = {a + b}')
+
+@bot.command()
+async def hello(ctx, count = 5):
+    await ctx.send('Hello!' * count)
+
+@bot.command()
+async def password(ctx, length: int):
+    lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    
+    all_characters = lowercase + uppercase + numbers
+
+    password = [random.choice(lowercase),
+                random.choice(uppercase),
+                random.choice(numbers)]
+    
+    while len(password) < length:
+        next_char = random.choice(all_characters)
+        if next_char not in password:
+            password.append(next_char)
+    
+    random.shuffle(password)
+    password = ''.join(password)
+
+    hashed = bcrypt.haspw(password.encode(), bcrypt.gensalt())
+
+    await ctx.send(f'Wygenerowne haslo: {password}, {hashed}')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit))for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def addition(ctx, *numbers: int):
+    await ctx.send(sum(numbers))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating....'):
+    for i in range(times):
+        await ctx.send(content)
+   
+
+
+bot.run('MTI2NjQzMjk2NjI0NzE5MDUzOQ.GCf4tM.zv0DEFYFtLDvktmJZI-CIZFOog1A_JV_zXxt_Q')
+
+
